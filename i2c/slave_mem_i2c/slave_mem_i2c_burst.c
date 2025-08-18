9d5cb34c11725a7db92b9d3f968431667b6ed981
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-08-18 09:55:08 +0100

    Fixups to support configurable boards

--- i2c/slave_mem_i2c/slave_mem_i2c_burst.c
+++ i2c/slave_mem_i2c/slave_mem_i2c_burst.c
@@ -17,8 +17,8 @@ static const uint I2C_BAUDRATE = 100000; // 100 kHz
 #ifdef i2c_default
 // For this example, we run both the master and slave from the same board.
 // You'll need to wire pin GP4 to GP6 (SDA), and pin GP5 to GP7 (SCL).
-static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
-static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
+static const uint I2C_SLAVE_SDA_PIN = 4;
+static const uint I2C_SLAVE_SCL_PIN = 5;
 static const uint I2C_MASTER_SDA_PIN = 6;
 static const uint I2C_MASTER_SCL_PIN = 7;
 
